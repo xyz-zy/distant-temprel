5ba28151a6b6a623c6223051f2887a5ff3c6cc37
Xinyu Zhao <xinyuzhao@utexas.edu>
2020-06-16 22:35:15 -0500

    refactor

--- load_data.py
+++ load_data.py
@@ -1,5 +1,6 @@
 import copy
 import glob
+import json
 import pickle
 import sys
 
@@ -11,14 +12,63 @@ from utils import convert_examples_to_features, apply_random_mask
 from transformers import RobertaTokenizer
 
 from DecompTime import parse_udst
-from beforeafter import examples as beforeafter
 from timebank.examples import MatresLoader
 
 sys.path.insert(1, 'Temporal-event-ordering/event_model')
 
+def get_beforeafter_examples(EXAMPLE_DIR="beforeafter_examples/", num_examples=None, ratio=False, during=False):
+    example_files = glob.glob(EXAMPLE_DIR + "*.json")
+
+    d = 0.04
+    a = 0.48
+    b = 0.48
+
+    exs = []
+
+    after_exs = []
+    before_exs = []
+    during_exs = []
+
+    for FILE in example_files:
+        print(FILE)
+        if not during and "during" in FILE:
+            continue
+        if ratio:
+            if "after" in FILE:
+                exs = after_exs
+            elif "before" in FILE:
+                exs = before_exs
+            elif "during" in FILE:
+                exs = during_exs
+            else:
+                continue
+
+        with open(FILE) as file:
+            exs_list = json.load(file)
+
+            for ex_json in exs_list:
+                example = IndexedExamplePartial.from_json(ex_json, doc_name=FILE)
+                exs.append(example)
+        if num_examples and not ratio and len(exs) >= num_examples:
+             break
+    if ratio:
+        if num_examples:
+            d = int(d * num_examples)
+            a = int(a * num_examples)
+            b = int(b * num_examples)
+            #print(d, a, b)
+            return during_exs[:d] + after_exs[:a] + before_exs[:b]
+        else:
+            ab_cap = min(len(after_exs), len(before_exs))
+            d_cap = int(ab_cap * 0.05)
+            #print(ab_cap, d_cap)
+            return during_exs[:d_cap] + after_exs[:ab_cap] + before_exs[:ab_cap]
+    else:
+        return exs[:num_examples]
+
 
 def beforeafter_examples(tokenizer, lm='roberta', ext='', num_examples=None, mask=False, during=True):
-    g_train_examples = beforeafter.get_examples(
+    g_train_examples = get_beforeafter_examples(
         EXAMPLE_DIR="beforeafter/examples" + ext + "/", num_examples=num_examples, during=during)
     if mask:
         mask = 'beforeafter'
